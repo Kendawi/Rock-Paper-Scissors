0da70ef6c4566410346aa362c16a172608c8950b
Kendawi <elmoizabdulazimkendawi@gmail.com>
2026-05-24 00:05:52 +0300

    add ui to the project

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,13 +1,5 @@
 
 
-
-function getHumanChoice() {
-    let humanChoice = prompt("please select a choice 1- Rock, 2- Paper, 3- Scissors");
-    humanChoice = humanChoice.toLowerCase();
-    return humanChoice
-}
-// this function return a random number from 1 to 3
-
 function getComputerChoice (min = 1, max = 3) {
     let computerChoice = Math.floor(Math.random() * (max - min + 1)) + min;
     computerChoice =  (computerChoice === 1) ? "rock" : (computerChoice === 2) ? "paper" : "scissors";
@@ -38,62 +30,99 @@ function playRound(humanChoice,computerChoice) {
 
 function playGame () {
 
-
-console.log("Welcome to Rock Paper Scissors Game..");
-
-
 let humanScore = 0;
 let computerScore = 0;
 
-let humanChoice;
 let computerChoice;
-
 let result = "";
 
-for (let i = 1; i <= 5;i++) {
+const container = document.createElement("div");
 
-    console.log("Round " + i + " :");
+const rockBtn = document.createElement("button");
+rockBtn.textContent = "rock";
 
-     humanChoice = getHumanChoice();
-     computerChoice = getComputerChoice();
+const paperBtn = document.createElement("button");
+paperBtn.textContent = "paper";
 
-    result = playRound(humanChoice,computerChoice);
+const scissorsBtn = document.createElement("button");
+scissorsBtn.textContent = "scissors";
 
-    if (result === "draw") {
-        console.log("It's draw this round!");
-        alert("It's draw this round!");
+
+container.appendChild(rockBtn);
+container.appendChild(paperBtn);
+container.appendChild(scissorsBtn);
+
+
+const gameResult = document.createElement("div");
+const roundResult = document.createElement("p");
+const finalResult = document.createElement("p");
+
+document.body.appendChild(container);
+document.body.appendChild(gameResult);
+
+
+const restartButton = document.createElement("button");
+restartButton.textContent = "Restart";
+
+
+function roundResultUi (result) {
+
+     if (result === "draw") {
+            roundResult.textContent = "It's draw this round!";
     }
     else if (result === "win") {
-        humanScore++;
-        console.log("You win this round :)");
-        alert("You win this round :)");
+        roundResult.textContent = "You win this round :)";
     }
     else {
+        roundResult.textContent = "You lose this round :'(";
+    }
+
+    gameResult.appendChild(roundResult);
+}
+
+
+function GameResultUi () {
+
+        finalResult.textContent = `GAME OVER!\n
+    You score : ${humanScore} round(s)\n
+    Computer score : ${computerScore} round(s)`; 
+
+        gameResult.appendChild(finalResult);
+        gameResult.appendChild(restartButton);
+        container.removeEventListener("click",handleClick);
+   
+}
+
+
+function handleClick (e) {
+
+    if (e.target.tagName === "BUTTON") {
+
+    computerChoice = getComputerChoice();
+        result = playRound(e.target.textContent,computerChoice);
+
+        if (result === "win")
+            humanScore++;       
+        else if (result === "lose")
         computerScore++;
-        console.log("You lose this round :'(");
-        alert("You lose this round :'(");
+     
+
+        roundResultUi(result);
+    }
+         
+
+    if (humanScore === 5 || computerScore === 5) {
+        GameResultUi();
     }
 }
 
-console.log("GAME OVER!");
-console.log("You score : " + humanScore + " rounds");
-console.log("Computer score : " + computerScore + " rounds");
 
-alert(`GAME OVER!\n
-    You score : ${humanScore} round(s)\n
-    Computer score : ${computerScore} round(s)`);
+container.addEventListener('click',handleClick);
 
-if (humanScore === computerScore) {
-    console.log("It's draw");
-}
-else if (humanScore >= computerScore) {
-    console.log("You win the game");
-}
-else{
-    console.log("You lose the game");
-}
-
-
+gameResult.addEventListener('click',(e) => {
+    if (e.target === restartButton)
+        location.reload();
+})
 }
 
 playGame();
